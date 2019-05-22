2a3f09e101e29db0b7f2bab4fdaf042694c2b0e3
Arthur Galiev <arthur.galiev4@gmail.com>
2019-05-22 23:56:19 +0300

    add searching by first/last name

--- src/main/java/app/dao/CustomerDAOImpl.java
+++ src/main/java/app/dao/CustomerDAOImpl.java
@@ -69,4 +69,37 @@ public class CustomerDAOImpl implements CustomerDAO {
         theQuery.executeUpdate();
 
     }
+
+    @Override
+    public List<Customer> searchCustomers(String theSearchName) {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        Query theQuery = null;
+
+        //
+        // only search by name if theSearchName is not empty
+        //
+
+        if(theSearchName != null && theSearchName.trim().length() > 0) {
+
+            // search for firstName or lastName ... case insensitive
+            theQuery = currentSession.createQuery("from Customer where lower(firstName) like :theName or" +
+                    " lower(lastName) like :theName");
+
+            theQuery.setParameter("theName", "%" + theSearchName.toLowerCase() + "%");
+
+        }
+
+        else {
+
+            // theSearchName is empty ... so just get all customers
+            theQuery = currentSession.createQuery("from Customer");
+        }
+
+        List<Customer> customers = theQuery.list();
+
+        return customers;
+    }
 }
